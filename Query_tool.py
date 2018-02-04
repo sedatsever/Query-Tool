9510a3270e6244de09ab9a95f7fbfd75867f98bf
sedatsever <33395965+sedatsever@users.noreply.github.com>
2018-02-05 02:03:42 +0300

    Add files via upload

--- Query_tool.py
+++ Query_tool.py
@@ -10,8 +10,9 @@ etree = ET.parse(r'C:\Users\Sedat\PycharmProjects\Smart Query\.idea\environments
 eroot = etree.getroot()
 
 class MyWindow(QtWidgets.QDialog):
-    def __init__(self):
-        super(MyWindow, self).__init__()
+    def __init__(self, fn=None,parent=None):
+        super(MyWindow, self).__init__(parent,\
+        flags = Qt.WindowMinimizeButtonHint | Qt.WindowMaximizeButtonHint | Qt.WindowCloseButtonHint )
         uic.loadUi('Smart_query_tool.ui', self)
 
         self.show()
@@ -21,18 +22,13 @@ class MyWindow(QtWidgets.QDialog):
 
         self.d_tableWidget.setContextMenuPolicy(Qt.CustomContextMenu)
 
-        self.q_textEdit.setEnabled(False)
-        self.table_combo.setEnabled(False)
-        self.columns_combo.setEnabled(False)
-        self.operator_combo.setEnabled(False)
-        self.cr_lineEdit.setEnabled(False)
-        self.generate_button.setEnabled(False)
-        self.run_button.setEnabled(False)
+        self.makepassive()
 
         self.cnxn_error_label.hide()
         self.qry_error_label.hide()
+        self.cnxn_label.hide()
 
-        self.q_textEdit.setText('Please connect to a server first to edit query')
+        self.q_textEdit.setText('Please connect to a server to generate or manually input a query')
 
         #Connect object functions
         self.server_combo.activated.connect(self.list_db)
@@ -43,33 +39,44 @@ class MyWindow(QtWidgets.QDialog):
         self.d_tableWidget.customContextMenuRequested.connect(self.openMenu)
 
     def list_db(self):
+        self.cnxn_error_label.hide()
         self.db_combo.clear()
-        db_connection = pyodbc.connect('Driver=SQL Server;''Server={a};''Database=gtpbrdb;''uid={b};pwd={c};'.format(
+
+        try:
+            db_connection = pyodbc.connect('Driver=SQL Server;''Server={a};''Database={j};''uid={b};pwd={c};'.format(
                                                                                   a = '{d}'.format(d = eroot.findall(
                                                                                       ".//*[@name='{e}']/connection".format(
                                                                                           e = self.server_combo.currentText()))[0].text),
                                                                                   b = '{f}'.format(f = eroot.findall(
                                                                                       ".//*[@name='{g}']/id".format(
                                                                                           g = self.server_combo.currentText()))[0].text),
+                                                                                  j = '{k}'.format(k = eroot.findall(
+                                                                                      ".//*[@name='{l}']/db".format(
+                                                                                          l = self.server_combo.currentText()))[0].text),
                                                                                   c = '{h}'.format(h = eroot.findall(
                                                                                       ".//*[@name='{i}']/pass".format(
-                                                                                          i = self.server_combo.currentText()))[0].text)))
+                                                                                          i = self.server_combo.currentText()))[0].text)),timeout=3)
+        except:
+            self.cnxn_error_label.show()
+            self.cnxn_label.hide()
+            self.makepassive()
 
-        dbcursor = db_connection.cursor()
+        else:
+            dbcursor = db_connection.cursor()
 
-        dbcursor.execute("select name from sys.databases (NOLOCK)")
+            dbcursor.execute("select name from sys.databases (NOLOCK)")
 
-        dbrows = dbcursor.fetchall()
+            dbrows = dbcursor.fetchall()
 
-        self.db_combo.addItems([row.name for row in dbrows])
+            self.db_combo.addItems([row.name for row in dbrows])
 
-        #db_connection.close()
+            self.cnxn_error_label.hide()
 
     def connect(self):
         global connection
-        global cursor
-        self.q_textEdit.clear()
-        connection = pyodbc.connect('Driver=SQL Server;''Server={a};''Database={b};''uid={c};pwd={d};'.format(
+
+        try:
+            connection = pyodbc.connect('Driver=SQL Server;''Server={a};''Database={b};''uid={c};pwd={d};'.format(
                                                                                   a='{e}'.format(e= eroot.findall(
                                                                                       ".//*[@name='{f}']/connection".format(
                                                                                           f=self.server_combo.currentText()))[0].text),
@@ -80,43 +87,30 @@ class MyWindow(QtWidgets.QDialog):
                                                                                   d = '{i}'.format(i=eroot.findall(
                                                                                       ".//*[@name='{j}']/pass".format(
                                                                                           j = self.server_combo.currentText()))[0].text)))
+        except:
+            self.qry_error_label.show()
+            self.cnxn_label.hide()
+            self.makepassive()
 
-        cursor = connection.cursor()
+        else:
+            global cursor
+            cursor = connection.cursor()
 
-        # while True:
-        #     row=cursor.fetchone()
-        #     if not row:
-        #         break
-        #     self.table_combo.addItems(i)
+            self.table_combo.clear()
 
-        # tables=[row.table_name for row in cursor.tables()]
+            self.table_combo.addItems([row.table_name for row in cursor.tables()])
 
-        # for row in cursor.tables():
-        #     tables.append(row.table_name)
-        #
-        self.table_combo.clear()
+            self.q_textEdit.setEnabled(True)
+            self.table_combo.setEnabled(True)
+            self.columns_combo.setEnabled(True)
+            self.operator_combo.setEnabled(True)
+            self.cr_lineEdit.setEnabled(True)
+            self.generate_button.setEnabled(True)
+            self.run_button.setEnabled(True)
 
-        self.table_combo.addItems([row.table_name for row in cursor.tables()])
-
-        self.q_textEdit.setEnabled(True)
-        self.table_combo.setEnabled(True)
-        self.columns_combo.setEnabled(True)
-        self.operator_combo.setEnabled(True)
-        self.cr_lineEdit.setEnabled(True)
-        self.generate_button.setEnabled(True)
-        self.run_button.setEnabled(True)
-
-        # columns=[]
-
-
-
-
-        # for i in self.gentables():
-        #     self.table_combo.addItems(i)
-
-        # self.columns_combo.addItems([column[0] for column in self.table_combo.currentText.description])
-        #
-        # self.operator_combo.addItems('=','>','<','>=','<=','NOT EQUAL TO','IN','LIKE')
+            self.qry_error_label.hide()
+            self.cnxn_label.show()
+            self.q_textEdit.clear()
 
     def list_columns(self):
         self.columns_combo.clear()
@@ -152,57 +146,34 @@ class MyWindow(QtWidgets.QDialog):
                                                                                                     criteria=self.cr_lineEdit.text()))
 
     def run(self):
-        # msg = QMessageBox()
-        # msg.setIcon(QMessageBox.Warning)
-        # msg.setText("Please check yor query")
-        # msg.setWindowTitle("Syntax Error")
-        # msg.setStandardButtons(QMessageBox.Ok)
-        # msg.buttonClicked.connect(self.connect())
+        try:
+            cursor.execute(self.q_textEdit.toPlainText())
 
-        # try:
-        #     cursor.execute(self.q_textEdit.toPlainText())
-        # except:
-        #     self.error_label.show()
-        #     self.connect()
-        # else:
-        #     self.error_label.hide()
+        except:
+            self.qry_error_label.show()
 
-        #     self.error_label.show
-        # else:
-        #     self.error_label.hide()
+        else:
+            qresults = cursor.fetchall()
 
-        self.error_label.show()
+            row_count=0
 
-        cursor.execute(self.q_textEdit.toPlainText())
+            for rows in iter(qresults):
+                row_count+=1
 
-        qresults = cursor.fetchall()
+            self.d_tableWidget.setRowCount(row_count)
 
-        row_count=0
+            column_count = len([column[0] for column in cursor.description])
 
-        for rows in iter(qresults):
-            row_count+=1
+            self.d_tableWidget.setColumnCount(column_count)
 
-        self.d_tableWidget.setRowCount(row_count)
+            self.d_tableWidget.setHorizontalHeaderLabels([column[0] for column in cursor.description])
 
-        column_count = len([column[0] for column in cursor.description])
+            for row in range(row_count):
+                for column in range(column_count):
+                    self.d_tableWidget.setItem(row, column, QTableWidgetItem(str(qresults[row][column])))
 
-        self.d_tableWidget.setColumnCount(column_count)
-
-        self.d_tableWidget.setHorizontalHeaderLabels([column[0] for column in cursor.description])
-
-        # self.d_tableWidget.setItem(1, 1, QTableWidgetItem(str(qresults[0][1])))
-
-        self.d_tableWidget.resizeColumnsToContents()
-
-        # self.d_tableWidget.setItem(row_count,0,qresults[1][0])
-
-        # for row in range(row_count):
-        #     for column in range(column_count):
-        #         self.d_tableWidget.setItem(row, column, QTableWidgetItem(QString("%1").arg(qresults[row][column])))
-
-        for row in range(row_count):
-            for column in range(column_count):
-                self.d_tableWidget.setItem(row, column, QTableWidgetItem(str(qresults[row][column])))
+            self.d_tableWidget.resizeColumnsToContents()
+            self.qry_error_label.hide()
 
     def openMenu(self,position):
         menu = QMenu()
@@ -229,42 +200,20 @@ class MyWindow(QtWidgets.QDialog):
             stream = io.StringIO()
             csv.writer(stream).writerows(table)
             QApplication.clipboard().setText(stream.getvalue())
-        # cb = QApplication.clipboard()
-        # cb.clear(mode=cb.Clipboard)
-        # cb.setText(str(self.d_tableWidget.selectedIndexes()), mode=cb.Clipboard)
-        #
-        # print(self.d_tableWidget.selectedItems())
 
     def copyheaders(self):
         headers_lst=[column[0] for column in cursor.description]
         headers_str=",".join(headers_lst)
         QApplication.clipboard().setText(headers_str)
 
-
-
-
-
-
-# class Session:
-#
-#     def __init__(self, server, db, uid, pwd):
-#         self.server=server
-#         self.db=db
-#         self.uid=uid
-#         self.pwd=pwd
-#
-#     def set_server(self,server):
-#         self.server=server
-#
-#     def set_db(self,db):
-#         self.db=db
-#
-#     def connect(self):
-#         return('Driver=SQL Server;''Server={a};''Database={b};''uid={c};pwd={d};'.format(a='212.252.33.21', b='gtpbrdb',
-#                                                                                        c='GTPDB', d='GTPDB'))
-
-    #def fill_db(self):
-
+    def makepassive(self):
+        self.q_textEdit.setEnabled(False)
+        self.table_combo.setEnabled(False)
+        self.columns_combo.setEnabled(False)
+        self.operator_combo.setEnabled(False)
+        self.cr_lineEdit.setEnabled(False)
+        self.generate_button.setEnabled(False)
+        self.run_button.setEnabled(False)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
